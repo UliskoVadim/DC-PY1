6542be8a94feb12242a9e716573cf9d3f7f079db
UliskoVadim <115507074+UliskoVadim@users.noreply.github.com>
2022-10-16 21:24:27 +0300

    Лабараторная работа №3

---
+++ Laba_3/task_1.py
@@ -0,0 +1,10 @@
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+# 1) True and True or False and False - Упростить not
+# 2) True or False - Упростить and
+# 3) True - Упростить or
+
+result = True
+
+print(src == result)
